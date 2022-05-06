124e8fa056b02c46cd877aeb7ed606417c3624ea
tim <email>
2022-05-06 15:51:29 +0200

    dirty fix

--- frontend/src/apis/axios.js
+++ frontend/src/apis/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const request = axios.create({
-  baseURL: "http://localhost"
+  // baseURL: "http://localhost"
+  baseUrl: "http://demomer-env.eba-srdm2jqy.us-east-1.elasticbeanstalk.com"
 });
 
 export default request;
