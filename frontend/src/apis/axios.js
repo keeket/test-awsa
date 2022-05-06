5f1cdb65949258db920834b95e0aadc42a38b603
tim <email>
2022-05-06 17:32:06 +0200

    werkt met defname?

--- frontend/src/apis/axios.js
+++ frontend/src/apis/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const request = axios.create({
   // baseURL: "http://localhost"
-  baseUrl: "http://demomer-env.eba-srdm2jqy.us-east-1.elasticbeanstalk.com"
+  // baseUrl: "http://demomer-env.eba-srdm2jqy.us-east-1.elasticbeanstalk.com"
+  baseURL: "http://mer-backend:8080"
 });
 
 export default request;
