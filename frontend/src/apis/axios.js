2a470b721bd1e8bda2835b3d747d2251869ea9ba
tim <email>
2022-05-07 19:21:49 +0200

    test dirty url opnieuw

--- frontend/src/apis/axios.js
+++ frontend/src/apis/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const request = axios.create({
   // baseURL: "http://localhost" // gebruik in lokale omgeving
-  // baseUrl: "http://demomer-env.eba-srdm2jqy.us-east-1.elasticbeanstalk.com"
-  baseURL: "http://mer-backend:8080"
+  baseUrl: "http://demomer-env.eba-srdm2jqy.us-east-1.elasticbeanstalk.com"
+  // baseURL: "http://mer-backend:8080"
 });
 
 export default request;
