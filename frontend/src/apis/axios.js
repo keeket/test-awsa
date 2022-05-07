03ff2f078a2520cc63c34d05461c5cbef9d8e80e
tim <email>
2022-05-07 18:55:51 +0200

    backend gebruikt java jar ipv maven spring-boot plugin

--- frontend/src/apis/axios.js
+++ frontend/src/apis/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-  // baseURL: "http://localhost"
+  // baseURL: "http://localhost" // gebruik in lokale omgeving
   // baseUrl: "http://demomer-env.eba-srdm2jqy.us-east-1.elasticbeanstalk.com"
   baseURL: "http://mer-backend:8080"
 });
